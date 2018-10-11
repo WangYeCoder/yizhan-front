953b858a8cd010db8c6516f16a29311103276d46
wangye <wangye@miot.cn>
2018-10-11 16:09:13 +0800

    first commit

---
+++ src/pages/area/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="area">
+    这是选择地区页面
+  </div>
+</template>
+
+<script>
+
+  import { formatTime } from "@/utils/index";
+  import card from "@/components/card";
+
+  export default {
+    data() {
+      return {
+        motto: "Hello World",
+        userInfo: {}
+      };
+    },
+
+    components: {
+      card
+    },
+    computed: {},
+    methods: {}
+  };
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    这是主页面
+  </div>
+</template>
+
+<script>
+
+  import { formatTime } from "@/utils/index";
+  import card from "@/components/card";
+
+  export default {
+    data() {
+      return {
+        motto: "Hello World",
+        userInfo: {}
+      };
+    },
+
+    components: {
+      card
+    },
+    computed: {},
+    methods: {}
+  };
+</script>
+
+<style>
+
+</style>
